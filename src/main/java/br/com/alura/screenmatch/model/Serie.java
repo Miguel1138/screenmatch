e2898e9046d05ad39b39f327253e771fedaaed05
Miguel S. da Silva <miguelss616@outlook.com.br>
2025-06-04 18:41:30 -0300

    Add fetch type for episodes
    - Used eager fetch type to load all the episodes every time serie class is instantiated.

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -24,7 +24,10 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(targetEntity = Episodio.class, mappedBy = "serie")
+    @OneToMany(targetEntity = Episodio.class,
+            mappedBy = "serie",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public List<Episodio> getEpisodios() {
@@ -33,6 +36,7 @@ public class Serie {
 
     public void setEpisodios(List<Episodio> episodios) {
         this.episodios = episodios;
+        episodios.forEach(e -> e.setSerie(this));
     }
 
     public Serie() {
